48c6b2466bf29ecec3a3e28a69d95110c61334b3
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-28 17:16:24 +0800

    something

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 
 
 export default function ProfilePage(props) {
     const location = useLocation();
+    let [token, setToken] = useState(null)
     useEffect(() => {
-        console.log(["pageview", location[1].hash]);
+        setToken(location.hash.split('&')[0].replace('#access_token=', ''));
     }, [location]);
-    console.log('here')
         // eslint-disable-next-line
         // , [])
+    
+    useEffect(() => getInfo()
+        // eslint-disable-next-line
+        , [token])
+    
+    
+    getInfo()
+    
+    
     return (
         <div>
             <h1>PROFILE</h1>
