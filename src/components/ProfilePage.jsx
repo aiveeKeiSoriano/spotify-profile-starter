b04fea872fa48aab55acf6aeeeaf249bca52b8d4
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-28 16:44:39 +0800

    location

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,10 +1,12 @@
-// import { useEffect } from "react"
+import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 
 
 export default function ProfilePage(props) {
-    const { hash } = useLocation();
-        console.log(hash)
+    const location = useLocation();
+    useEffect(() => {
+        console.log(["pageview", location.pathname]);
+      }, [location]);
         // eslint-disable-next-line
         // , [])
     return (
