f3f7677e4f4a37af714609dfc6e849fdadc390d9
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-28 16:22:54 +0800

    useffect warning

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,7 +2,9 @@ import { useEffect } from "react"
 
 
 export default function ProfilePage(props) {
-    useEffect(() => console.log(props.match.params.token), [])
+    useEffect(() => console.log(props.match.params.token)
+        // eslint-disable-next-line
+        , [])
     return (
         <div></div>
     )
