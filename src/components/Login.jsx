bfdb4611f1b2955bc28041263b47d9324608d2b6
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-28 15:21:39 +0800

    change url

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,12 +4,14 @@ import { useEffect } from "react"
 export default function Login() {
 
     useEffect(() => {
-        fetch('https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f%0D%0A&response_type=token&redirect_uri=www.google.com').then(resp => resp.json()).then(data => console.log(data)).catch((e) => console.log(e))
+        fetch('https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&redirect_uri=https://quizzical-poitras-057011.netlify.app/').then(resp => resp.json()).then(data => console.log(data)).catch((e) => console.log(e))
     }, [])
 
     // async function getToken() {
     //     let response = await fetch({
-    //         method: 
+    //         method: 'GET',
+    //         url: 'https://accounts.spotify.com/authorize'
+
     //     })
     // }
 
