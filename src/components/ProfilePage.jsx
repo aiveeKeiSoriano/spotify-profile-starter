56bc49c26532c76d71e9aacec49a38be1ab813d7
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-28 16:22:29 +0800

    useffect

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 
 
 export default function ProfilePage(props) {
-    useEffect(() => console.log(props.params.match.token), [])
+    useEffect(() => console.log(props.match.params.token), [])
     return (
         <div></div>
     )
