583aef68af77b82dc3534299ebc6d3683ae75088
Nithya <nithyaasworld@gmail.com>
2021-05-28 17:17:52 +0530

    styled the home page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,34 @@
-// import { useEffect } from "react"
-
-
+import styled from 'styled-components'
 export default function Login() {
 
-    // useEffect(() => {
-    //     fetch('https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&redirect_uri=https://quizzical-poitras-057011.netlify.app/').then(resp => resp.json()).then(data => console.log(data)).catch((e) => console.log(e))
-    // }, [])
-
-    // useEffect(() => getToken(), [])
-
-    // async function getToken() {
-    //     let response = await fetch({
-    //         method: 'POST',
-    //         url: 'https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&redirect_uri=https://quizzical-poitras-057011.netlify.app/'
-
-    //     })
-    //     // let data = await response.json()
-    //     console.log(response)
-    // }
-
+    const LoginContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 1.2em;
+    justify-content: center;
+    height: 100vh;
+    background-color: #181818;
+    `
+    const H1Tag = styled.h1`
+    font-size: 1.5em;
+    color: white;
+    `
+    const LogInBtn = styled.a`
+    background-color: #1FBA57;
+    color: white;
+    padding: 1em 2em;
+    border-radius: 50px;
+    text-decoration: none;
+    font-weight: bold;
+    display: inline-block;
+    `;
 
     return (
-        <div className="login">
-            <button><a href='https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&scope=user-top-read%20user-read-private%20user-read-email&redirect_uri=http://localhost:3000/profile'>Login</a></button>
-        </div>
+        <LoginContainer>
+            <H1Tag>Spotify Profile</H1Tag>
+            <LogInBtn href='https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&scope=user-top-read%20user-read-private%20user-read-email&redirect_uri=http://localhost:3000/profile'>
+               LOG IN TO SPOTIFY </LogInBtn>
+        </LoginContainer>
     )
 }
