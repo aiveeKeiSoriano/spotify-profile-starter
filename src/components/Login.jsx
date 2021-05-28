4980c0389f68878b75759b8df6d4d4edf6921e7e
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-28 16:09:42 +0800

    try route

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,22 +3,26 @@ import { useEffect } from "react"
 
 export default function Login() {
 
-    useEffect(() => {
-        fetch('https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&redirect_uri=https://quizzical-poitras-057011.netlify.app/').then(resp => resp.json()).then(data => console.log(data)).catch((e) => console.log(e))
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&redirect_uri=https://quizzical-poitras-057011.netlify.app/').then(resp => resp.json()).then(data => console.log(data)).catch((e) => console.log(e))
+    // }, [])
+
+    // useEffect(() => getToken(), [])
 
     // async function getToken() {
     //     let response = await fetch({
-    //         method: 'GET',
-    //         url: 'https://accounts.spotify.com/authorize'
+    //         method: 'POST',
+    //         url: 'https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&redirect_uri=https://quizzical-poitras-057011.netlify.app/'
 
     //     })
+    //     // let data = await response.json()
+    //     console.log(response)
     // }
 
 
     return (
         <div className="login">
-            <button>Login</button>
+            <button><a href='https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&redirect_uri=https://quizzical-poitras-057011.netlify.app/profile/'>Login</a></button>
         </div>
     )
 }
--- src/components/RouteWrapper.jsx
+++ src/components/RouteWrapper.jsx
@@ -1,10 +1,21 @@
 
 import Login from './Login'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import ProfilePage from './ProfilePage';
 
 export default function RouteWrapper() {
     return (
-        <div>
-            <Login />
-        </div>
+        <Router>
+                <Switch>
+                    <Route path={`/profile/:token`} component={ProfilePage} />
+                    <Route path='/'>
+                        <Login />
+                    </Route>
+                </Switch>
+        </Router>
     )
 }
