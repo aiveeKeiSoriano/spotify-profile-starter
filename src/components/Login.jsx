8402f1f2abfdbce8df2cfb115c4c7ce5f6ee68f5
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-06-03 23:59:44 +0800

    logged in

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,40 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
 import styled from 'styled-components'
-export default function Login() {
 
-    const LoginContainer = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    gap: 1.2em;
-    justify-content: center;
-    height: 100vh;
-    background-color: #181818;
-    `
-    const H1Tag = styled.h1`
-    font-size: 2rem;
-    color: white;
-    `
-    const LogInBtn = styled.a`
-    background-color: #1FBA57;
-    color: white;
-    padding: .8em 1.3em;
-    border-radius: 50px;
-    text-decoration: none;
-    font-weight: bold;
-    display: inline-block;
-    letter-spacing: 1.5px;
-    `;
+const LoginContainer = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+gap: 1.2em;
+justify-content: center;
+height: 100vh;
+background-color: #181818;
+`
+const H1Tag = styled.h1`
+font-size: 2rem;
+color: white;
+`
+const LogInBtn = styled.a`
+background-color: #1FBA57;
+color: white;
+padding: .8em 1.3em;
+border-radius: 50px;
+text-decoration: none;
+font-weight: bold;
+display: inline-block;
+letter-spacing: 1.5px;
+`;
+
+export default function Login() {
 
     //implicit grant
     // const URL = 'https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=token&scope=user-top-read%20user-read-private%20user-read-email&redirect_uri=http://localhost:3000/profile'
-    
+
     //auth code - aivee app
     // const URL = 'https://accounts.spotify.com/authorize?client_id=b6b96f827f584079959eae2bd104c04f&response_type=code&scope=user-top-read%20user-read-private%20user-read-email&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fprofile'
-    
-    
+
+
     const endpoint = 'https://accounts.spotify.com/authorize?'
     const clientID = 'client_id=e7bebc81234e4737ada60bc666eb50d7'
     const response = '&response_type=code'
@@ -39,9 +42,18 @@ export default function Login() {
 
     // localhost redirect
     // const redirect = '&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fprofile'
-    
+
     //netlify redirect
     const redirect = '&redirect_uri=https%3A%2F%2Fquizzical-poitras-057011.netlify.app%2Fprofile'
+    
+    const LocalToken = localStorage.getItem('Token')
+    let history = useHistory()
+
+    useEffect(() => {
+        if (LocalToken) history.push('/profile')
+    }
+        // eslint-disable-next-line
+        , [])
 
     return (
         <LoginContainer>
